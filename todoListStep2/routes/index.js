a45a9c568ef0bca0515da4f9ff82a98cffe5dba1
terawiseman <terawiseman@gmail.com>
2015-04-08 09:45:26 +0900

    Revert "notComp"
    This reverts commit 2bf9f755660bc9a5d6e96c4cbe5e97e90b6a514b.

--- todoListStep2/routes/index.js
+++ todoListStep2/routes/index.js
@@ -1,17 +1,11 @@
-/************************************
-  index.js
-  サイトルートに来た時のルーティング
-  ＴＯＰページへのアクセス
-  date:2015.04.08
-*************************************/
-
 var express = require('express');
 var router = express.Router();
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TodoList' });
+  console.log("index.js");
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
